f7efcc37f920994ace54de2c9a5669c36638247f
Klem26 <Kankina26@gmail.com>
2021-09-07 19:17:18 +0300

    create  router -users, userSchema,

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+
+
+const userSchema = Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength:6,
+    
+  },
+  email: {
+    type: String,
+    unique:true,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+}, { versionKey: false, timestamps: true });
+
+
+const joiUserSchema = Joi.object({
+    password: Joi.string().min(6).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    subscription: Joi.string(),
+    token: Joi.string(),
+})
+
+const User = model("user", userSchema );
+
+module.exports = {
+    User,
+    joiUserSchema
+};
