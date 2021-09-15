d075fc73b0e746f979503830158f145505b94af2
Klem26 <Kankina26@gmail.com>
2021-09-15 12:30:55 +0300

    updet ctrl  signup, login. Sending a verification email.

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -2,15 +2,17 @@ const bcrypt = require("bcryptjs");
 const fs = require("fs/promises");
 const path = require("path");
 const gravatar = require("gravatar");
+const { v4 } = require('uuid');
 
-
+const {sendMail} =require('../../utils')
 const HTTP_CODS = require("../../helpers/httpCodes");
 const { User } = require("../../models");
 const imageDir = path.join(__dirname, "../../", "public/images");
 
 const signup = async (req, res) => {
     const { email, password } = req.body;
-    const defaultImage = gravatar.url({email}, { s: 250 }, true);
+    const verifyToken = v4();
+    const defaultImage = gravatar.url({ email }, { s: 250 }, true);
     const user = await User.findOne({email});
     if (user) {
         return res.status(HTTP_CODS.CONFLICT)
@@ -22,15 +24,25 @@ const signup = async (req, res) => {
     }
 
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    const result = await User.create({email ,image:`https:${defaultImage}`, password: hashPassword});
+    const result = await User.create({ email, image: `https:${defaultImage}`, password: hashPassword, verifyToken});
     const id = result._id.toString();
     const dirPath = path.join(imageDir, id);
     await fs.mkdir(dirPath)
+
+
+    const data = {
+    to: email ,
+    subject: "Confirm email",
+    html:` <a href="http://localhost:3000/api/users/verify/${verifyToken}">Confirm email</a>`
+    }
+    await sendMail(data)
+    
     res.status(HTTP_CODS.CREATED)
         .json({
         status: "success",
         code: 201,
         message: "Success register"
+        // html:` <a href="http://localhost:3000/api/users/verify/${verifyToken}">Confirm email</a>`
     })
 };
 
