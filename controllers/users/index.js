2fc0ffacd40ce0237a4896b5675e4d9160f1c0b8
Klem26 <Kankina26@gmail.com>
2021-09-15 12:28:14 +0300

    create  controller verify

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,11 +2,12 @@ const signup = require("./signup");
 const login = require("./login");
 const logout = require("./logout");
 const updateImg = require("./updateImg");
-
+const verify = require('./verify');
 module.exports = {
     signup,
     login ,
     logout,
-    updateImg 
+    updateImg,
+    verify
 }
 
