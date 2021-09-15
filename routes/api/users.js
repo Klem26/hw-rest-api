4624f9c164088939d2b44dccdb9685fe693d6f77
Klem26 <Kankina26@gmail.com>
2021-09-15 12:25:35 +0300

    add new router verify/:verifyToken , update User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,6 +10,7 @@ router.post("/signup",validationMiddleware,controllerWrapper(ctrl.signup));
 router.post("/login",validationMiddleware, controllerWrapper(ctrl.login));
 router.get("/logout", controllerWrapper(authenticate), controllerWrapper(ctrl.logout));
 router.patch("/:id", upload.single("image"), controllerWrapper(ctrl.updateImg));
+router.get("/verify/:verifyToken", controllerWrapper(ctrl.verify));
 
 module.exports = router;
 
